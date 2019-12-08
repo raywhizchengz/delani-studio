86a0f7463a4a1b1cf0be1b1f69e22a6698438274
Raychengole97 <raychengole97@gmail.com>
2019-12-08 11:10:32 +0300

    add footer icons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-    
-}
+$(document).ready(function() {
+    $(".design").hide();
+    $(".design").click(function() {
+        $(".design").toggle();
+        $(".design img").toggle();
+    });
+    $(".develop").hide();
+    $(".develop").click(function() {
+        $(".develop").toggle();
+        $(".develop img").toggle();
+    });
+    $(".product management").hide();
+    $(".product management").click(function() {
+        $(".product management").toggle();
+        $(".product management img").toggle();
+    });
+});
