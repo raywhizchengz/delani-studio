0c7966b7d2d6d42c20851cb88d46c3264261df4b
Raychengole97 <raychengole97@gmail.com>
2019-12-10 07:04:51 +0300

    resolved conflicts

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
-    $("#imagedesign").on("click",function(){
-        $("#des-m").show();
+    $("#imagedesign").click(function(){
         $("#imagedesign").hide();
+        $("#des-m").toggle();
+        $("#des-m").show();        
     });
-    $("#des-m").on("click", function(){
-        $("imagedesign").show();
-            $("des-m").hide();
+    $("#des-m").click(function(){
+        $("#des-m").hide();
+        $("#imagedesign").toggle();
+        $("#imagedesign").show();        
     });
-    
+});
+$(document).ready(function(){
+    $("#imagedev").click(function(){
+        $("#imagedev").hide();
+        $("#dev-m").toggle();
+        $("#dev-m").show();        
+    });
+    $("#dev-m").click(function(){
+        $("#dev-m").hide();
+        $("#imagedev").toggle();
+        $("#imagedev").show();        
+    });
+});
+$(document).ready(function(){
+    $("#imageprod").click(function(){
+        $("#imageprod").hide();
+        $("#prod-m").toggle();
+        $("#prod-m").show();        
+    });
+    $("#prod-m").click(function(){
+        $("#prod-m").hide();
+        $("#imageprod").toggle();
+        $("#imageprod").show();        
+    });
+
+    $("#subscribe").click(function(){
+       alert("Thank you for subscribing to our channel");
+    });
 });
