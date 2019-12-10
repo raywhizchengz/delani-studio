630eb140b70cb1430df15a43001352fd3f55cea6
raywhizchengz <57936681+raywhizchengz@users.noreply.github.com>
2019-12-10 07:12:23 +0300

    add toglling effect

--- js/script.js
+++ js/script.js
@@ -1,65 +1,41 @@
  
-//For toggling on WHAT WE DO
-$(document).ready(function() {
-    $(".onCrack1").click(function() {
-        $("#click1").toggle();
-        $("#magic1").toggle();
+$(document).ready(function(){
+    $("#imagedesign").click(function(){
+        $("#imagedesign").hide();
+        $("#des-m").toggle();
+        $("#des-m").show();        
     });
-    $(".onCrack2").click(function() {
-        $("#magic2").toggle();
-        $("#click2").toggle();
-    });
-    $(".onCrack3").click(function() {
-        $("#magic3").toggle();
-        $("#click3").toggle();
+    $("#des-m").click(function(){
+        $("#des-m").hide();
+        $("#imagedesign").toggle();
+        $("#imagedesign").show();        
     });
 });
-
-// For hover effect on PORTFOLIO
-$(document).ready(function() {
-    $("div.image-frame").hover(function() {
-        $(".classAdd1").addClass("mask flex-center rgba-red-strong");
+$(document).ready(function(){
+    $("#imagedev").click(function(){
+        $("#imagedev").hide();
+        $("#dev-m").toggle();
+        $("#dev-m").show();        
     });
-    $("div.image-frame").hover(function() {
-        $(".classAdd2").addClass("mask flex-center rgba-blue-light");
-    });
-    $("div.image-frame").hover(function() {
-        $(".classAdd3").addClass("mask flex-center rgba-green-slight");
-    });
-    $("div.image-frame").hover(function() {
-        $(".classAdd4").addClass("mask flex-center");
+    $("#dev-m").click(function(){
+        $("#dev-m").hide();
+        $("#imagedev").toggle();
+        $("#imagedev").show();        
     });
 });
-
-var btn = document.getElementById("myBtn");
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function(event) {
-    // Validate form for entry
-    var x = document.forms["myForm"]["contact-name"].value;
-    if (x == "") {
-        alert("Name not entered");
-        return false;
-    }
-    var y = document.forms["myForm"]["contact-email"].value;
-    if (y == "") {
-        alert("Email not entered");
-        return false;
-    }
-    var z = document.forms["myForm"]["contact-message"].value;
-    if (z == "") {
-        alert("You have no message to send");
-        return false;
-    }
-    var person1Input = $("input#contact-name").val();
-    $("span#outputName").append(person1Input);
-
-    $('.toast').toast('show');
-    event.preventDefault();
-}
-
-      $("#mc-embedded-subscribe").on("click",function() {
-          alert("ftydrjfygfutdrdytdytdyd");
-      })
-      
+$(document).ready(function(){
+    $("#imageprod").click(function(){
+        $("#imageprod").hide();
+        $("#prod-m").toggle();
+        $("#prod-m").show();        
     });
+    $("#prod-m").click(function(){
+        $("#prod-m").hide();
+        $("#imageprod").toggle();
+        $("#imageprod").show();        
+    });
+
+    $("#subscribe").click(function(){
+       alert("Thank you for subscribing to our channel");
+    });
+});
